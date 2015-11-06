2d7ec988dcc3812cae30ee8e9feb0d60636a8636
Anatoly Dryga <anatolydryga@gmail.com>
2015-11-06 13:30:36 -0500

    code rearangment for refactoring

--- epigeneticHeatMapMaker.R
+++ epigeneticHeatMapMaker.R
@@ -1,33 +1,13 @@
-make_epi_heatmap <- function(sampleName_GTSP, referenceGenome, output_dir, connection, annotation) {
-    freeze <- referenceGenome
-    insfile.name <- output_dir
+libs <- c("survival", "pipeUtils", "colorspace", "hiAnnotator", 
+          "plyr", "reshape2")
+loaded <- sapply(libs, library, character.only=TRUE, quietly=TRUE)
 
-    windows <- c('10000')
+make_epi_heatmap <- function(sampleName_GTSP, referenceGenome, output_dir, connection, annotation) {
     if ( ! "label" %in% colnames(sampleName_GTSP)) {
         sampleName_GTSP$label <- sampleName_GTSP$GTSP
     }
     sampleName_GTSP <- select(sampleName_GTSP, sampleName, GTSP, label)
     sampleName_GTSP$refGenome <- rep(referenceGenome, nrow(sampleName_GTSP))
-    setName <- sampleName_GTSP$sampleName
-    Alias <- sampleName_GTSP$label
-
-    # datasets
-    meth.sets <- c('H2BK5me1','H3R2me1','H3R2me2','H3K4me1','H3K4me2','H3K4me3','H3K9me1','H3K9me2','H3K9me3','H3K27me1','H3K27me2','H3K27me3','H3K36me1','H3K36me3','H3K79me1','H3K79me2','H3K79me3','H4R3me2','H4K20me1','H4K20me3','PolII','H2AZ','CTCF','H3K4me1-HeLaS3_Untr','H3K4me1-HeLaS3_INFS','H3K4me3-HeLaS3_Untr','H3K4me3-HeLaS3_INFS','H3K27me3-CD133','H3K4me1-CD133','H3K9me1-CD133','H4K20me1-CD133','H3K27me1-CD133','H3K36me3-CD133','H3K4me3-CD133','H3K9me3-CD133','PolII-CD133','H2AZ-CD133','H3K9me2-HeLa','H3K9me3-HeLa','H3K27me3-HeLa','H3K36me3-HeLa','H4K20me3-HeLa','CTCF-HeLa','H4K20me1-HeLa','H2BK5me1-HeLa','H3K4me1-HeLa','H2AZ_hsalt-HeLa','H2AZ_lsalt-HeLa','H3K4me3-HeLa')
-    act.sets <- c('H2AK5ac','H2AK9ac','H2BK120ac','H2BK12ac','H2BK20ac','H2BK5ac','H3K14ac','H3K18ac','H3K23ac','H3K27ac','H3K36ac','H3K4ac','H3K9ac','H4K12ac','H4K16ac','H4K5ac','H4K8ac','H4K91ac','H3K27ac-HeLa') 
-    tf.sets <- c('NRSF','STAT1-HeLaS3_Untr','STAT1-HeLaS3_INFS','STAT1-HeLaS3_INFS-2','ActivatedNucleosomes','RestingNucleosomes','Rest-CD4-HDAC1','Rest-CD4-HDAC2','Rest-CD4-HDAC3','Rest-CD4-HDAC6','Act-CD4-HDAC6','Rest-CD4-CBP','Rest-CD4-PCAF','Rest-CD4-p300','Rest-CD4-MOF','Rest-CD4-Tip60','Act-CD4-Tip60','H3_3-HeLa','p300-HeLa')
-    inoutNuc <- 'yes'
-
-    #### Load required libraries & functions ####
-    libs <- c("survival", "pipeUtils", "colorspace", "hiAnnotator", 
-              "plyr", "reshape2")
-    loaded <- sapply(libs, library, character.only=TRUE, quietly=TRUE)
-
-    #### set parameters for annotations & analysis ####
-    dbConn <- connection
-    annotPath <- annotation
-    if( ! file.exists(annotPath)) {
-        stop("Required annotation directory doesn't exist: ", annotPath)
-    }
 
     add_label <- function(sites, sampleName_GTSP) {
         sites_GTSP <- merge(sites, sampleName_GTSP)
@@ -49,7 +29,30 @@ make_epi_heatmap <- function(sampleName_GTSP, referenceGenome, output_dir, conne
     sites_mrcs <- rbind(sites, mrcs)
 
     sites <- makeGRanges(sites_mrcs, soloStart=TRUE, 
-        chromCol="chr", strandCol="strand", startCol='position', freeze=freeze)
+        chromCol="chr", strandCol="strand", startCol='position', freeze=referenceGenome)
+
+    setName <- sampleName_GTSP$sampleName
+    Alias <- sampleName_GTSP$label
+
+    freeze <- referenceGenome
+    insfile.name <- output_dir
+
+    windows <- c('10000')
+
+    # datasets
+    meth.sets <- c('H2BK5me1','H3R2me1','H3R2me2','H3K4me1','H3K4me2','H3K4me3','H3K9me1','H3K9me2','H3K9me3','H3K27me1','H3K27me2','H3K27me3','H3K36me1','H3K36me3','H3K79me1','H3K79me2','H3K79me3','H4R3me2','H4K20me1','H4K20me3','PolII','H2AZ','CTCF','H3K4me1-HeLaS3_Untr','H3K4me1-HeLaS3_INFS','H3K4me3-HeLaS3_Untr','H3K4me3-HeLaS3_INFS','H3K27me3-CD133','H3K4me1-CD133','H3K9me1-CD133','H4K20me1-CD133','H3K27me1-CD133','H3K36me3-CD133','H3K4me3-CD133','H3K9me3-CD133','PolII-CD133','H2AZ-CD133','H3K9me2-HeLa','H3K9me3-HeLa','H3K27me3-HeLa','H3K36me3-HeLa','H4K20me3-HeLa','CTCF-HeLa','H4K20me1-HeLa','H2BK5me1-HeLa','H3K4me1-HeLa','H2AZ_hsalt-HeLa','H2AZ_lsalt-HeLa','H3K4me3-HeLa')
+    act.sets <- c('H2AK5ac','H2AK9ac','H2BK120ac','H2BK12ac','H2BK20ac','H2BK5ac','H3K14ac','H3K18ac','H3K23ac','H3K27ac','H3K36ac','H3K4ac','H3K9ac','H4K12ac','H4K16ac','H4K5ac','H4K8ac','H4K91ac','H3K27ac-HeLa') 
+    tf.sets <- c('NRSF','STAT1-HeLaS3_Untr','STAT1-HeLaS3_INFS','STAT1-HeLaS3_INFS-2','ActivatedNucleosomes','RestingNucleosomes','Rest-CD4-HDAC1','Rest-CD4-HDAC2','Rest-CD4-HDAC3','Rest-CD4-HDAC6','Act-CD4-HDAC6','Rest-CD4-CBP','Rest-CD4-PCAF','Rest-CD4-p300','Rest-CD4-MOF','Rest-CD4-Tip60','Act-CD4-Tip60','H3_3-HeLa','p300-HeLa')
+    inoutNuc <- 'yes'
+
+
+    #### set parameters for annotations & analysis ####
+    dbConn <- connection
+    annotPath <- annotation
+    if( ! file.exists(annotPath)) {
+        stop("Required annotation directory doesn't exist: ", annotPath)
+    }
+
 
     #species <- switch(freeze, hg18="Homo sapiens", mm8="Mus musculus")
     #sites <- keepStandardChromosomes(sites, style="UCSC", species=species)
